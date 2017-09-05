c78022a2f355c7a095474be0119d3dfb4c3b0b72
邱亚涛 <qiuyatao@117sport.com>
2017-09-05 14:55:07 +0800

    rn0.48.1 adapter

--- android/src/main/java/com/zyu/ReactNativeWheelPickerPackage.java
+++ android/src/main/java/com/zyu/ReactNativeWheelPickerPackage.java
@@ -20,11 +20,6 @@ public class ReactNativeWheelPickerPackage implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Arrays.<ViewManager>asList(
                 new ReactWheelCurvedPickerManager()
