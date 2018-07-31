7444056c004935d3e06ffda8729b357b0f8ec87a
Daniel Vispo Blog <daniel@vispo.es>
2018-07-31 17:54:37 +0900

    - IOS recovered

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-'use strict';
 
-import React from 'react';
-
-import WheelCurvedPicker from './WheelCurvedPicker'
-
-module.exports = (WheelCurvedPicker)
+import React from 'react'; 
+ 
+import { 
+  PickerIOS, 
+  Platform, 
+} from 'react-native'; 
+ 
+import WheelCurvedPicker from './WheelCurvedPicker' 
+ 
+module.exports = (Platform.OS === 'ios' ? PickerIOS : WheelCurvedPicker) 
+module.exports = (WheelCurvedPicker) 
