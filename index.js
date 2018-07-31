ebc2e88c9b73f9b6408c41f14f22a8e4a71713b3
Daniel Vispo Blog <daniel@vispo.es>
2018-07-31 17:29:08 +0900

    - Removed IOS

--- index.js
+++ index.js
@@ -2,11 +2,6 @@
 
 import React from 'react';
 
-import {
-	PickerIOS,
-	Platform,
-} from 'react-native';
-
 import WheelCurvedPicker from './WheelCurvedPicker'
 
-module.exports = (Platform.OS === 'ios' ? PickerIOS : WheelCurvedPicker)
+module.exports = (WheelCurvedPicker)
